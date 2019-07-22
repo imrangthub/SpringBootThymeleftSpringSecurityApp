b8f6b5c0f525c19d2a140a76d8f06e6b4338f478
MD IMRAN HOSSAIN <imranmadbar@gmail.com>
2019-07-22 16:03:50 +0600

    Init priject

---
+++ src/main/java/com/madbarsoft/springbootthymeleftspringsecurity/SpringBootThymeleftSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.madbarsoft.springbootthymeleftspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleftSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeleftSpringSecurityApplication.class, args);
+	}
+
+}
