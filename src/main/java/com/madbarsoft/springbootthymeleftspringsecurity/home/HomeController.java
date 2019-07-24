ace22d8fba143cbf1b6d03fb54b7ecfbcb49ee9c
MD IMRAN HOSSAIN <imranmadbar@gmail.com>
2019-07-24 11:20:57 +0600

    add Login Form

--- src/main/java/com/madbarsoft/springbootthymeleftspringsecurity/home/HomeController.java
+++ src/main/java/com/madbarsoft/springbootthymeleftspringsecurity/home/HomeController.java
@@ -1,13 +1,25 @@
 package com.madbarsoft.springbootthymeleftspringsecurity.home;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.madbarsoft.springbootthymeleftspringsecurity.book.BookEntity;
+import com.madbarsoft.springbootthymeleftspringsecurity.book.BookService;
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	private BookService bookService;
+	
 	@RequestMapping(value="/home")
-	public String home() {
+	public String home(Model model) {
+		List<BookEntity> bookList = bookService.list();
+		model.addAttribute("bookList", bookList);
 		return "home";
 	}
 
