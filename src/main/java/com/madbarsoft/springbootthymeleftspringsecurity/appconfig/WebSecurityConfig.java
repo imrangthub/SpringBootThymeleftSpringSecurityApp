262243ce6c183db052d9bc7fee2c4596e7578289
MD IMRAN HOSSAIN <imranmadbar@gmail.com>
2019-07-30 15:46:19 +0600

    Complete In-Memory Authencation with Spring Security

--- src/main/java/com/madbarsoft/springbootthymeleftspringsecurity/appconfig/WebSecurityConfig.java
+++ src/main/java/com/madbarsoft/springbootthymeleftspringsecurity/appconfig/WebSecurityConfig.java
@@ -1,16 +1,19 @@
 package com.madbarsoft.springbootthymeleftspringsecurity.appconfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {   // For Im - Memory Auth
 	
 	 @Override
 	  public void configure(WebSecurity web) throws Exception {
@@ -25,25 +28,45 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-             .authorizeRequests()
-     		 .antMatchers("/book/**").hasRole("USER")	
-             .antMatchers("/", "/home").permitAll()
-             .antMatchers("/login").permitAll()
-             .anyRequest().authenticated()
-             .and()
-             .formLogin().loginPage("/login").permitAll()
-	         .and()
-	         .logout() .permitAll()
-	         .and()
+	         .authorizeRequests()
+	     		 .antMatchers("/book/**").hasRole("USER")	
+	             .antMatchers("/", "/home").permitAll()
+	             .antMatchers("/login").permitAll()
+	             .anyRequest().authenticated()
+	             .and()
+             .formLogin()
+	             .loginPage("/login")
+	             .defaultSuccessUrl("/home")
+	             .permitAll()
+	             .and()
+	         .logout() 
+	             .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+	             .logoutSuccessUrl("/home")
+		         .permitAll()
+	             .and()
 	         .exceptionHandling().accessDeniedPage("/403");
     }
     
+	
 	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-			.inMemoryAuthentication()
-				.withUser("imran").password("123").roles("USER");
+	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+	    BCryptPasswordEncoder encoder = passwordEncoder();
+	    auth.inMemoryAuthentication()
+	    .withUser("imran").password(encoder.encode("123")).roles("USER")
+	    .and()
+	    .withUser("admin").password("12345").roles("USER", "ADMIN");
 	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
+	}
+	
+	
+//	 @Bean
+//	 public static NoOpPasswordEncoder passwordEncoder() {
+//	  return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+//	 }
 
 
 }
